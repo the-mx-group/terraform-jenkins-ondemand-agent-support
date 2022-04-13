991b4dd903b2f8c475e785ef106ce50a1d2815af
eric vz <evonzee@themxgroup.com>
2022-04-13 09:09:15 -0500

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,16 @@
 output "security_group_name" {
-  value = ""
+  value = aws_security_group.jenkins-build-agent.name
   description = "The name of the security group for nodes"
 }
-output "ssh_private_key" {
-  value = ""
-  description = "The SSH private key to use to connect to the nodes.  If you provided a public key input, this will be blank."
-}
 output "instance_profile" {
-  value = ""
+  value = aws_iam_instance_profile.jenkins-build-agent.arn
   description = "The ARN of the instance profile to be used on nodes"
 }
 output "access_key" {
-  value = ""
-  description = "The IAM access key which Jenkins will use to create instances"
+  value = aws_iam_access_key.jenkins-build-agent-manager.id
+  description = "The IAM access key ID which Jenkins will use to create instances"
 }
 output "access_secret" {
-  value = ""
+  value = aws_iam_access_key.jenkins-build-agent-manager.secret
   description = "The IAM secret which Jenkins will use to create instances"
 }
